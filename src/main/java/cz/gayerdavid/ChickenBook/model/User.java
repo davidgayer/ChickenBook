01aa5da27b70302132e2a33c08f66f43466adbab
DavidGayer <davidgayer1@gmail.com>
2024-01-19 20:40:15 +0100

    Added Lombok constructors/getters/setters and JPA db annotations

--- src/main/java/cz/gayerdavid/ChickenBook/model/User.java
+++ src/main/java/cz/gayerdavid/ChickenBook/model/User.java
@@ -1,22 +1,50 @@
 package cz.gayerdavid.ChickenBook.model;
 
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@Table(name = "user")
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Getter
+@Setter
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
 
+    @NonNull
+    @Column(name = "name", nullable = false)
     private String name;
 
+    @NonNull
+    @Column(name = "email", nullable = false)
     private String email;
 
+    @NonNull
+    @Column(name = "password", nullable = false)
     private String password;
 
+    @Column(name = "city")
     private String city;
 
+    @Column(name = "bio")
     private String bio;
 
+    @Column(name = "profile_picture")
     private Byte[] profilePicture;
 
     
