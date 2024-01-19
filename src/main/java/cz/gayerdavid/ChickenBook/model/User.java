6b065ee14596fd58ace83d16b231649ff5a39dfa
DavidGayer <davidgayer1@gmail.com>
2024-01-19 20:27:04 +0100

    Added atributtes of User.java class

--- src/main/java/cz/gayerdavid/ChickenBook/model/User.java
+++ src/main/java/cz/gayerdavid/ChickenBook/model/User.java
@@ -5,7 +5,19 @@ import jakarta.persistence.Entity;
 @Entity
 public class User {
 
-    private Long Id;
-    
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private String password;
+
+    private String city;
+
+    private String bio;
+
+    private Byte[] profilePicture;
+
     
 }
