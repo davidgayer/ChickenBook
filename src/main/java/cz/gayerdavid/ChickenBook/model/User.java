69e4d779317db0508317ddb8c018a85de9b196dc
DavidGayer <davidgayer1@gmail.com>
2024-01-20 18:15:28 +0100

    Updated User.java attributes/added list of friends with mapping to database

--- src/main/java/cz/gayerdavid/ChickenBook/model/User.java
+++ src/main/java/cz/gayerdavid/ChickenBook/model/User.java
@@ -1,11 +1,17 @@
 package cz.gayerdavid.ChickenBook.model;
 
 
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -43,6 +49,14 @@ public class User {
 
     @Column(name = "bio")
     private String bio;
+
+    @Column(name = "friends")
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(
+    name = "user_friends",
+    joinColumns = @JoinColumn(name = "user_id"),
+    inverseJoinColumns = @JoinColumn(name = "friend_id"))
+    private List<User> friends;
 
     
 
