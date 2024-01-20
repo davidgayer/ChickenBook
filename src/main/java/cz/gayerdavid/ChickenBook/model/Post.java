c42d621c454b4785fc4e92950ee236cee09f879c
DavidGayer <davidgayer1@gmail.com>
2024-01-20 19:51:45 +0100

    Created Post.java model/entity class

---
+++ src/main/java/cz/gayerdavid/ChickenBook/model/Post.java
@@ -0,0 +1,53 @@
+package cz.gayerdavid.ChickenBook.model;
+
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "post")
+@RequiredArgsConstructor
+@Getter
+@Setter
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "post-content")
+    private String content;
+
+    @Column(name = "image-url")
+    private String imageUrl;
+
+    @Column(name = "timestamp")
+    @JsonFormat(pattern = "dd/MM/yyyy, HH:mm:ss")
+    private LocalDateTime timestamp;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+
+
+
+    @PrePersist
+    protected void onCreate() {
+        timestamp = LocalDateTime.now();
+    }
+    
+}
