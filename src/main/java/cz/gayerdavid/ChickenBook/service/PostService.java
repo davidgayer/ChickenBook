e15d9416d2c4b7a384891104a54a8d2268a8885b
DavidGayer <davidgayer1@gmail.com>
2024-01-20 20:31:25 +0100

    Created PostService.java interface

---
+++ src/main/java/cz/gayerdavid/ChickenBook/service/PostService.java
@@ -0,0 +1,14 @@
+package cz.gayerdavid.ChickenBook.service;
+
+import java.util.List;
+
+import cz.gayerdavid.ChickenBook.model.Post;
+
+public interface PostService {
+
+    Post getPost(Long id);
+    List<Post> getAllPosts();
+    Post savePost(Post post);
+    void deletePost(Long Id);
+    
+}
