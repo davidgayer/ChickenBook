c4fed469f2d27abb66fd8e474c0798b820ccfc94
DavidGayer <davidgayer1@gmail.com>
2024-01-20 11:54:10 +0100

    Created UserServiceImpl.java class

---
+++ src/main/java/cz/gayerdavid/ChickenBook/service/UserServiceImpl.java
@@ -0,0 +1,36 @@
+package cz.gayerdavid.ChickenBook.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import cz.gayerdavid.ChickenBook.model.User;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Override
+    public User getUser(Long id) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public User saveUser(User user) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void deleteUser(Long id) {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
