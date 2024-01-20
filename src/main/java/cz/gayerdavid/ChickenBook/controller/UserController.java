d44ba91d2639ed14db6b7d7fbcc37a3ca05ceec7
DavidGayer <davidgayer1@gmail.com>
2024-01-20 14:28:29 +0100

    Created new UserController.java for User class and basic rest methods

---
+++ src/main/java/cz/gayerdavid/ChickenBook/controller/UserController.java
@@ -0,0 +1,49 @@
+package cz.gayerdavid.ChickenBook.controller;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import cz.gayerdavid.ChickenBook.model.User;
+import cz.gayerdavid.ChickenBook.service.UserService;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    UserService userService;
+
+    @GetMapping("{id}")
+    public ResponseEntity<User> getUser(@PathVariable Long id) {
+        return new ResponseEntity<>(userService.getUser(id), HttpStatus.OK);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<User>> getAllUsers() {
+        return new ResponseEntity<>(userService.getAllUsers(), HttpStatus.OK);
+        
+    }
+
+    @PostMapping()
+    public ResponseEntity<User> saveUser(@RequestBody User user) {
+        return new ResponseEntity<>(userService.saveUser(user), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<HttpStatus> deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    
+}
