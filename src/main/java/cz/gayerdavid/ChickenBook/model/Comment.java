09c27288a96b8d0d158198711e32c6e274d9db3b
DavidGayer <davidgayer1@gmail.com>
2024-01-23 16:01:43 +0100

    Created new entity Comment.java

---
+++ src/main/java/cz/gayerdavid/ChickenBook/model/Comment.java
@@ -0,0 +1,16 @@
+package cz.gayerdavid.ChickenBook.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table
+@RequiredArgsConstructor
+@Getter
+@Setter
+public class Comment {
+    
+}
