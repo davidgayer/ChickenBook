433c224cfd205c307207d615da39736c28a7edd7
DavidGayer <davidgayer1@gmail.com>
2024-01-23 17:18:09 +0100

    Added method findAllByPostId to CommentRepository interface

--- src/main/java/cz/gayerdavid/ChickenBook/repository/CommentRepository.java
+++ src/main/java/cz/gayerdavid/ChickenBook/repository/CommentRepository.java
@@ -1,9 +1,13 @@
 package cz.gayerdavid.ChickenBook.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import cz.gayerdavid.ChickenBook.model.Comment;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    List<Comment> findAllByPostId(Long postId);
     
 }
