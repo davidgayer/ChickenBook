05df056497083e7f04604155da482cc80e68b648
DavidGayer <davidgayer1@gmail.com>
2024-01-23 15:24:42 +0100

    Delete findByUserId method from PostRepository

--- src/main/java/cz/gayerdavid/ChickenBook/repository/PostRepository.java
+++ src/main/java/cz/gayerdavid/ChickenBook/repository/PostRepository.java
@@ -1,7 +1,5 @@
 package cz.gayerdavid.ChickenBook.repository;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import cz.gayerdavid.ChickenBook.model.Post;
@@ -9,6 +7,5 @@ import cz.gayerdavid.ChickenBook.model.Post;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
-    Optional<Post> findByUserId(Long id);
     
 }
