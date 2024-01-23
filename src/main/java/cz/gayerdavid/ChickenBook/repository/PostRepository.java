517b9bbaef53ee113999944baf6ab40d0bb51951
DavidGayer <davidgayer1@gmail.com>
2024-01-23 12:46:58 +0100

    Added findByUserId method into PostRepository interface

--- src/main/java/cz/gayerdavid/ChickenBook/repository/PostRepository.java
+++ src/main/java/cz/gayerdavid/ChickenBook/repository/PostRepository.java
@@ -1,10 +1,14 @@
 package cz.gayerdavid.ChickenBook.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import cz.gayerdavid.ChickenBook.model.Post;
 
 
 public interface PostRepository extends JpaRepository<Post, Long> {
+
+    Optional<Post> findByUserId(Long id);
     
 }
