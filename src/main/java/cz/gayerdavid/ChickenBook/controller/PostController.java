07f0511f41eedd44c9c4f02da6101b97d3d13c68
DavidGayer <davidgayer1@gmail.com>
2024-01-23 15:50:12 +0100

    Created PostController.java

---
+++ src/main/java/cz/gayerdavid/ChickenBook/controller/PostController.java
@@ -0,0 +1,54 @@
+package cz.gayerdavid.ChickenBook.controller;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import cz.gayerdavid.ChickenBook.model.Post;
+import cz.gayerdavid.ChickenBook.service.PostService;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/post")
+public class PostController {
+
+    PostService postService;
+
+    @GetMapping("/{postId}")
+    public ResponseEntity<Post> getPost(@PathVariable Long postId) {
+        return new ResponseEntity<>(postService.getPost(postId), HttpStatus.OK);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Post>> getAllPosts() {
+        return new ResponseEntity<>(postService.getAllPosts(), HttpStatus.OK);
+    }
+
+    @GetMapping("/user/{userId}")
+    public ResponseEntity<List<Post>> getAllUserPosts(@PathVariable Long userId) {
+        return new ResponseEntity<>(postService.getAllUserPosts(userId), HttpStatus.OK);
+    }
+
+    @PostMapping("/user/{userId}")
+    public ResponseEntity<Post> savePost(@RequestBody Post post, @PathVariable Long userId) {
+        return new ResponseEntity<>(postService.savePost(post, userId), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<HttpStatus> deletePost(@PathVariable Long id) {
+        postService.deletePost(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    
+    
+}
