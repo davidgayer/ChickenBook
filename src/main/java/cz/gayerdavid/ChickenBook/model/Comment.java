cfc3ce48d227a5d5f9928e25717ec1babea5b589
DavidGayer <davidgayer1@gmail.com>
2024-01-23 16:18:25 +0100

    Added attributes for Comment entity

--- src/main/java/cz/gayerdavid/ChickenBook/model/Comment.java
+++ src/main/java/cz/gayerdavid/ChickenBook/model/Comment.java
@@ -1,6 +1,17 @@
 package cz.gayerdavid.ChickenBook.model;
 
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -12,5 +23,30 @@ import lombok.Setter;
 @Getter
 @Setter
 public class Comment {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "comment_content")
+    private String content;
+
+    @Column(name = "timestamp")
+    @JsonFormat(pattern = "dd/MM/yyyy, HH:mm:ss")
+    private LocalDateTime timestamp;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id", referencedColumnName = "id")
+    private Post post;
+
+    @PrePersist
+    protected void onCreate() {
+        timestamp = LocalDateTime.now();
+    }
+
 }
