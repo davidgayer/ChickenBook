b31f126cd9b5855df86bfb03933b3c1a2ce5da5d
DavidGayer <davidgayer1@gmail.com>
2024-01-24 23:14:29 +0100

    Created MessageService interface and implementation

---
+++ src/main/java/cz/gayerdavid/ChickenBook/service/MessageService.java
@@ -0,0 +1,17 @@
+package cz.gayerdavid.ChickenBook.service;
+
+import java.util.List;
+
+import cz.gayerdavid.ChickenBook.model.Message;
+
+public interface MessageService {
+
+    Message getMessage(Long messageId);
+
+    List<Message> getAllUserMessages(Long userId);
+
+    Message sendMessage(Message message, Long senderId, Long receiverId);
+
+}
+    
+
---
+++ src/main/java/cz/gayerdavid/ChickenBook/service/MessageServiceImpl.java
@@ -0,0 +1,50 @@
+package cz.gayerdavid.ChickenBook.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import cz.gayerdavid.ChickenBook.exception.UserNotFoundException;
+import cz.gayerdavid.ChickenBook.model.Message;
+import cz.gayerdavid.ChickenBook.model.User;
+import cz.gayerdavid.ChickenBook.repository.MessageRepository;
+import cz.gayerdavid.ChickenBook.repository.UserRepository;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class MessageServiceImpl implements MessageService {
+
+    private final MessageRepository messageRepository;
+    private final UserRepository userRepository;
+
+    @Override
+    public Message getMessage(@NonNull Long messageId) {
+        return messageRepository.findById(messageId).get();
+    }
+
+    @Override
+    public List<Message> getAllUserMessages(Long userId) {
+        return messageRepository.findAllByUserId(userId);
+    }
+
+    @Override
+    public Message sendMessage(Message message, @NonNull Long senderId, @NonNull Long receiverId) {
+        Optional<User> sender = userRepository.findById(senderId);
+        if (sender.isPresent()) {
+            message.setSender(sender.get());
+        } else {
+            throw new UserNotFoundException(senderId);
+        }
+        Optional<User> receiver = userRepository.findById(receiverId);
+        if (receiver.isPresent()) {
+            message.setReceiver(receiver.get());
+        } else {
+            throw new UserNotFoundException(receiverId);
+        }
+
+        return messageRepository.save(message);
+    }
+}
