e93b63f3f0734b63c63872e6de3f58bc26cd51f6
DavidGayer <davidgayer1@gmail.com>
2024-01-24 15:31:51 +0100

    Added method findAllByPostId to LikeRepository

--- src/main/java/cz/gayerdavid/ChickenBook/repository/LikeRepository.java
+++ src/main/java/cz/gayerdavid/ChickenBook/repository/LikeRepository.java
@@ -1,9 +1,13 @@
 package cz.gayerdavid.ChickenBook.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import cz.gayerdavid.ChickenBook.model.Like;
 
 public interface LikeRepository extends JpaRepository<Like, Long> {
+
+    List<Like> findAllByPostId(Long postId);
     
 }
