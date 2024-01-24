63ad3478e9914da89180994643039740a1649f98
DavidGayer <davidgayer1@gmail.com>
2024-01-24 23:15:47 +0100

    Added method for find all messages by user id

--- src/main/java/cz/gayerdavid/ChickenBook/repository/MessageRepository.java
+++ src/main/java/cz/gayerdavid/ChickenBook/repository/MessageRepository.java
@@ -1,9 +1,13 @@
 package cz.gayerdavid.ChickenBook.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import cz.gayerdavid.ChickenBook.model.Message;
 
 public interface MessageRepository extends JpaRepository<Message, Long> {
+
+    List<Message> findAllByUserId(Long userId);
     
 }
