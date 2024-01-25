bc20079e402453a7c73553223026567d981a4e4a
DavidGayer <davidgayer1@gmail.com>
2024-01-25 15:49:41 +0100

    Deleted findAllByReceiver method of MessageRepository

--- src/main/java/cz/gayerdavid/ChickenBook/repository/MessageRepository.java
+++ src/main/java/cz/gayerdavid/ChickenBook/repository/MessageRepository.java
@@ -1,13 +1,8 @@
 package cz.gayerdavid.ChickenBook.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import cz.gayerdavid.ChickenBook.model.Message;
 
-public interface MessageRepository extends JpaRepository<Message, Long> {
-
-    List<Message> findAllByReceiverId(Long userId);
-    
+public interface MessageRepository extends JpaRepository<Message, Long> {    
 }
