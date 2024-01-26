e89c672bda740912172afc50eb3506f107353ee2
DavidGayer <davidgayer1@gmail.com>
2024-01-26 20:33:54 +0100

    Deleted unused Exception classes which was replaced by EntityNotFoundException

--- src/main/java/cz/gayerdavid/ChickenBook/exception/PostNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package cz.gayerdavid.ChickenBook.exception;
-
-public class PostNotFoundException extends RuntimeException {
-
-    public PostNotFoundException(Long id) {
-        super("The post id '" + id + "' not found.");
-    }
-
-}
--- src/main/java/cz/gayerdavid/ChickenBook/exception/UserNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package cz.gayerdavid.ChickenBook.exception;
-
-public class UserNotFoundException extends RuntimeException {
-
-    public UserNotFoundException(Long id) {
-        super("The user id '" + id + "' not found.");
-    }
-    
-}
