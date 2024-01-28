d3d0f7d36ab49b5a2c9bdb50bda9dfddf6c151f9
DavidGayer <davidgayer1@gmail.com>
2024-01-28 22:23:07 +0100

    Created class SecurityConstants

---
+++ src/main/java/cz/gayerdavid/ChickenBook/Security/SecurityConstants.java
@@ -0,0 +1,9 @@
+package cz.gayerdavid.ChickenBook.Security;
+
+public class SecurityConstants {
+    public static final String SECRET_KEY = "ax67:ajLKIZ541a214;&ahpj";
+    public static final int TOKEN_EXPIRATION = 3600;
+    public static final String BEARER = "Bearer";
+    public static final String AUTHORIZATION = "Authorization";
+    public static final String REGISTER_PATH = "/user/register";
+}
