f1bce55a01f982f1dbbbfc8c0a6ab2fd48769594
DavidGayer <davidgayer1@gmail.com>
2024-01-30 13:21:54 +0100

    Recreated method registerUser in the UserService interface

--- src/main/java/cz/gayerdavid/ChickenBook/service/UserService.java
+++ src/main/java/cz/gayerdavid/ChickenBook/service/UserService.java
@@ -2,11 +2,11 @@ package cz.gayerdavid.ChickenBook.service;
 
 import java.util.List;
 
-import cz.gayerdavid.ChickenBook.exception.WrongPasswordFormatException;
 import cz.gayerdavid.ChickenBook.model.User;
 
 public interface UserService {
-    User getUser(Long userId)
+    User getUser(Long userId);
+    User registerUser(User user);
     void deleteUser(Long userId);
     List<User> getAllUsers();
 }
