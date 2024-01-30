88acb717872dda4b00882b3605cc16518aa07058
DavidGayer <davidgayer1@gmail.com>
2024-01-30 11:35:19 +0100

    Deleted unused imports

--- src/main/java/cz/gayerdavid/ChickenBook/service/UserService.java
+++ src/main/java/cz/gayerdavid/ChickenBook/service/UserService.java
@@ -2,11 +2,11 @@ package cz.gayerdavid.ChickenBook.service;
 
 import java.util.List;
 
+import cz.gayerdavid.ChickenBook.exception.WrongPasswordFormatException;
 import cz.gayerdavid.ChickenBook.model.User;
 
 public interface UserService {
-    User getUser(Long userId);
-    User registerUser(User user);
+    User getUser(Long userId)
     void deleteUser(Long userId);
     List<User> getAllUsers();
 }
