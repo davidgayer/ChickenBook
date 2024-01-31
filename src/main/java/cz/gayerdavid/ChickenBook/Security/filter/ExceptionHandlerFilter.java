e5297a744f1ce164f69944524724ff375023836c
DavidGayer <davidgayer1@gmail.com>
2024-01-31 18:40:19 +0100

    Created security filter for handle the exceptions, handles EntityNotFoundException for non existing user and other runtime exceptions

---
+++ src/main/java/cz/gayerdavid/ChickenBook/Security/filter/ExceptionHandlerFilter.java
@@ -0,0 +1,32 @@
+package cz.gayerdavid.ChickenBook.security.filter;
+
+import java.io.IOException;
+
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import cz.gayerdavid.ChickenBook.exception.EntityNotFoundException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+//First filter hitted with request, handless exceptions and errors
+public class ExceptionHandlerFilter extends OncePerRequestFilter { //used once per request
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        try {
+            filterChain.doFilter(request, response); //if everything is ok, move to the next filter (authentication)
+        } catch (EntityNotFoundException exception) { //if user is not found in the database, response with 404 Not Found
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            response.getWriter().write("Username doesn't exist.");
+            response.getWriter().flush();
+        } catch (RuntimeException exception) { // if other Runtime exception is catched response with 400 Bad Request
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            response.getWriter().write("BAD REQUEST");
+            response.getWriter().flush();
+        } 
+    }
+
+}
