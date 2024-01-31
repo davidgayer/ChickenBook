b80767bc069706eb977aa3907f06306763f26df3
DavidGayer <davidgayer1@gmail.com>
2024-01-31 18:44:51 +0100

    Added ExceptionHandlerFilter into securityFilterChain

--- src/main/java/cz/gayerdavid/ChickenBook/Security/SecurityConfig.java
+++ src/main/java/cz/gayerdavid/ChickenBook/Security/SecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import cz.gayerdavid.ChickenBook.security.filter.AuthenticationFilter;
+import cz.gayerdavid.ChickenBook.security.filter.ExceptionHandlerFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -27,14 +28,15 @@ public class SecurityConfig {
         authenticationFilter.setFilterProcessesUrl("/authenticate"); //Set on which path authenticationFilter works
 
         http
-                .headers(headers -> headers.frameOptions(frame -> frame.disable())) // Allows frame on h2 console view
+                .headers(headers -> headers.frameOptions(frame -> frame.disable())) // Allows correct view of h2 database in the web browser
                 .csrf(csrf -> csrf.disable()) // cross site request forgery (disabled because of use of JWT token)
                 .authorizeHttpRequests(auth -> auth // Handles which path are authenticated and how.
                         .requestMatchers("/h2/**").permitAll() // Permit's access on path "/h2/**"
                         .requestMatchers(HttpMethod.POST, SecurityConstants.REGISTER_PATH).permitAll() //Permits acces on registration path saved at SecurityConstants
                         .anyRequest().authenticated()) // any other request, except the ones noted at requestMatchers
                                                        // nedds to be authenticated
-                .addFilter(authenticationFilter)
+                .addFilterBefore(new ExceptionHandlerFilter(), AuthenticationFilter.class) //First filter checking for exceptions
+                .addFilter(authenticationFilter) //This filter handles authentication of user
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)); // Preventing
                                                                                                               // of
                                                                                                               // creating
