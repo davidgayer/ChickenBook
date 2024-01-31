d0023c03eebe81d34762873356f022f7cc4719a8
DavidGayer <davidgayer1@gmail.com>
2024-01-31 21:29:55 +0100

    Add methods for successful and unsuccessful authentication

--- src/main/java/cz/gayerdavid/ChickenBook/Security/filter/AuthenticationFilter.java
+++ src/main/java/cz/gayerdavid/ChickenBook/Security/filter/AuthenticationFilter.java
@@ -11,6 +11,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import cz.gayerdavid.ChickenBook.model.User;
 import cz.gayerdavid.ChickenBook.security.manager.CustomAuthenticationManager;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -23,13 +25,35 @@ public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
             throws AuthenticationException {
-                try {
-                    User user = new ObjectMapper().readValue(request.getInputStream(), User.class);//Reads user input and save the informations into new user object
-                    Authentication authenticationObject = new UsernamePasswordAuthenticationToken(user.getEmail(), user.getPassword());//Create new authentication token which will be send to authentication check
-                    return customAuthenticationManager.authenticate(authenticationObject); //returns authentication token to Authentication manager
-                } catch (IOException exception) {
-                    throw new RuntimeException();
-                }
+        try {
+            User user = new ObjectMapper().readValue(request.getInputStream(), User.class);// Reads user input and save
+                                                                                           // the informations into new
+                                                                                           // user object
+            Authentication authenticationObject = new UsernamePasswordAuthenticationToken(user.getEmail(),
+                    user.getPassword());// Create new authentication token which will be send to authentication check
+            return customAuthenticationManager.authenticate(authenticationObject); // returns authentication token to
+                                                                                   // Authentication manager
+        } catch (IOException exception) {
+            throw new RuntimeException();
+        }
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
+            Authentication authResult) throws IOException, ServletException {
+        response.setStatus(HttpServletResponse.SC_OK);
+        response.getWriter().write("Success");
+        response.getWriter().flush();
+        System.out.println("GOOD WORK");
+    }
+
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response,
+            AuthenticationException failed) throws IOException, ServletException {
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.getWriter().write("Wrong password");
+        response.getWriter().flush();
+        System.out.println("THIS IS BAD");
     }
 
 }
