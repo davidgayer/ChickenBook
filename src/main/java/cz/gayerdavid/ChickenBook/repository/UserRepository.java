f6f59a6c7aa31fa808fa4ec72c3ae3d6f2ba4c89
DavidGayer <davidgayer1@gmail.com>
2024-01-31 21:30:27 +0100

    Add method for find user by email

--- src/main/java/cz/gayerdavid/ChickenBook/repository/UserRepository.java
+++ src/main/java/cz/gayerdavid/ChickenBook/repository/UserRepository.java
@@ -3,7 +3,10 @@ package cz.gayerdavid.ChickenBook.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import cz.gayerdavid.ChickenBook.model.User;
+import java.util.Optional;
+
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
     
 }
