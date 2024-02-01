820de73e83e131b76bb1d385b3cc889003b64e31
DavidGayer <davidgayer1@gmail.com>
2024-02-01 20:28:45 +0100

    Added token creation

--- src/main/java/cz/gayerdavid/ChickenBook/Security/filter/AuthenticationFilter.java
+++ src/main/java/cz/gayerdavid/ChickenBook/Security/filter/AuthenticationFilter.java
@@ -1,15 +1,19 @@
 package cz.gayerdavid.ChickenBook.security.filter;
 
 import java.io.IOException;
+import java.util.Date;
 
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import cz.gayerdavid.ChickenBook.model.User;
+import cz.gayerdavid.ChickenBook.security.SecurityConstants;
 import cz.gayerdavid.ChickenBook.security.manager.CustomAuthenticationManager;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -41,16 +45,18 @@ public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
     @Override
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
             Authentication authResult) throws IOException, ServletException {
-        response.setStatus(HttpServletResponse.SC_OK);
-        response.getWriter().write("Success");
-        response.getWriter().flush();
+        String token = JWT.create()
+                .withSubject(authResult.getName())
+                .withExpiresAt(new Date(System.currentTimeMillis() + SecurityConstants.TOKEN_EXPIRATION))
+                .sign(Algorithm.HMAC512(SecurityConstants.SECRET_KEY));
+            response.addHeader(SecurityConstants.AUTHORIZATION, SecurityConstants.BEARER + token);
     }
 
     @Override
     protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response,
             AuthenticationException failed) throws IOException, ServletException {
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-        response.getWriter().write("Wrong password");
+        response.getWriter().write(failed.getMessage());
         response.getWriter().flush();
     }
 
